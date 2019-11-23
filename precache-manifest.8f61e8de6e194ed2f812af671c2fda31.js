c2a76a03df65aba159a9de2ce13767817667584b
Ayush James <ayush.james1996@gmail.com>
2019-11-23 20:36:35 +1100

    Updates

---
+++ precache-manifest.8f61e8de6e194ed2f812af671c2fda31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41af8f377f0a1e23071e8c4848a4fa1",
+    "url": "/fireDash/index.html"
+  },
+  {
+    "revision": "5f3de8dcd6c71ce6958d",
+    "url": "/fireDash/static/css/main.e2b319b1.chunk.css"
+  },
+  {
+    "revision": "1ef68b8bb1de1abdce89",
+    "url": "/fireDash/static/js/2.529cb75d.chunk.js"
+  },
+  {
+    "revision": "5f3de8dcd6c71ce6958d",
+    "url": "/fireDash/static/js/main.d81abce3.chunk.js"
+  },
+  {
+    "revision": "0a006dbf6f8a968a640c",
+    "url": "/fireDash/static/js/runtime-main.85e25e77.js"
+  },
+  {
+    "revision": "ea7cc9d7d38afdcbc84f1a4e1eead22b",
+    "url": "/fireDash/static/media/airrow.ea7cc9d7.png"
+  },
+  {
+    "revision": "917141374436665b9de3849961654c06",
+    "url": "/fireDash/static/media/firechart.91714137.png"
+  }
+]);
